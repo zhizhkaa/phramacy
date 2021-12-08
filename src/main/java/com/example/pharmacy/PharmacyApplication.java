dc3a6f0ccf0d3d940c93ced21a8fed532022a05f
zhizhka <antonchekk@yandex.ru>
2021-12-08 14:42:15 +0300

    Created simple auth

--- src/main/java/com/example/pharmacy/PharmacyApplication.java
+++ src/main/java/com/example/pharmacy/PharmacyApplication.java
@@ -7,12 +7,13 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+
+public class PharmacyApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+
+        FXMLLoader fxmlLoader = new FXMLLoader(PharmacyApplication.class.getResource("/authorization.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
         stage.show();
     }
