b5efa379f1f690082a92a4789c835375269be80c
zhizhka <antonchekk@yandex.ru>
2021-12-09 21:12:50 +0300

    Доделал базовый функционал авторизации

--- src/main/java/com/pharmacy/AuthController.java
+++ src/main/java/com/pharmacy/AuthController.java
@@ -1,29 +1,54 @@
 package com.pharmacy;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Optional;
 
 public class AuthController {
-    @FXML private Label notification;
-    @FXML private TextField userName;
+    @FXML private Label notification;   // Для отладки
 
-    @FXML private PasswordField passwordHide;
-    @FXML private TextField passwordShown;
+    @FXML private TextField userName;           // Поле логин
+    @FXML private PasswordField passwordHide;   // Поле пароля (точки вместо текста)
+    @FXML private TextField passwordShown;      // Поле пароля (текст)
 
-    @FXML private Button showButton;
+    @FXML private VBox authPane;
 
-    // Если true - активно passwordShown
-    // Если false - активно passwordHide
+    // Если true - активно поле passwordShown
+    // Если false - активно поле passwordHide
     private boolean showPassword = false;
 
-    //TODO Сделать окно "Забыл пароль"
-    //protected void onForgotAction()
+    @FXML
+    protected void onForgotPassword() {
+        ButtonType resetPassword = new ButtonType("Сбросить пароль", ButtonBar.ButtonData.OK_DONE);
+        Alert alert = new Alert(Alert.AlertType.INFORMATION, null, resetPassword);
+        alert.setTitle("Забыл пароль");
+        alert.setHeaderText(null);
+        Label forgotLabel = new Label("Пожалуйста, обратитесь к вашему системному" +
+                                        "\nадминистратору для восстановления доступа или смены пароля.\n" +
+                                        "\nИли сбросьте пароль посредством\nпривязанной к аккаунту электронной почты:");
+        forgotLabel.setWrapText(true);
+        alert.getDialogPane().setContent(forgotLabel);
+
+        Optional<ButtonType> result = alert.showAndWait();
+
+        // Если закрыли окно
+        if (result.isEmpty()) {
+            notification.setText("Вышли из уведомления");
+        }
+        else if (result.get() == resetPassword) {
+            notification.setText("\"Отправили\" уведомление");
+            // TODO Отправить какое-нибдуь уведомление для сброса пароля
+        }
+
+    }
 
     @FXML
-    protected void onSignUpButtonPress() {
+    protected void onSignUpButtonPress() throws IOException {
         //TODO Сделать авторизацию
         // хранить будем в формате логин:пароль:ид_в_БД
 
@@ -35,35 +60,48 @@ public class AuthController {
         else {userName.setPromptText("Логин");}
 
         // Проверка на ввод пароля
-        if (showPassword && passwordShown.getText().trim().isEmpty()) { passwordShown.setPromptText("Вы не ввели пароль"); }
+        if (showPassword && passwordShown.getText().trim().isEmpty()) {
+            passwordShown.setPromptText("Вы не ввели пароль");
+        }
         else {
             passwordShown.setPromptText("Пароль");
             password = passwordShown.getText();
         }
-        if (!showPassword && passwordHide.getText().trim().isEmpty()) { passwordHide.setPromptText("Вы не ввели пароль"); }
+        if (!showPassword && passwordHide.getText().trim().isEmpty()) {
+            passwordHide.setPromptText("Вы не ввели пароль");
+        }
         else {
             passwordHide.setPromptText("Пароль");
             password = passwordHide.getText();
         }
-        notification.setText(password);
 
+        // TODO если хеш пароля совпадает с хешем в файле у пользователя и если есть такой клиент
         if (!login.isEmpty() && !password.isEmpty()) {
-            if (password.equals("user")) { notification.setText("Успех"); } // TODO если хеш пароля совпадает с хешем в файле у пользователя и если есть такой клиент
+            if (password.equals("user")) {
+                notification.setText("Успех");
 
+                Stage stage = (Stage) authPane.getScene().getWindow();
+                FXMLLoader fxmlLoader = new FXMLLoader(PharmacyApplication.class.getResource("/main.fxml"));
+
+                stage.hide();
+
+                SplitPane root = fxmlLoader.load();
+                stage.getScene().setRoot(root);
+                stage.setTitle("ИС \"Фармацевтическая компания\"");
+                stage.show();
+            }
+            else { notification.setText("Неправильный пароль"); }
 
         }
         // Проверка логина и пароля
         // TODO тут нужен какой нибудь BufferedReader чтобы читать из файла
-
-
-
     }
 
     @FXML
     protected void onShowButtonPress() {
-        // При нажатии на кнопку с глазиком получает пароль
-        // из активного поля и вставляет его в неактивное
-        // Так же меняет видимость полей на противоположную
+        /*  При нажатии на кнопку с глазиком получает пароль
+            из активного поля и вставляет его в неактивное
+            Так же меняет видимость полей на противоположную */
 
         String password;
 
@@ -71,7 +109,7 @@ public class AuthController {
             password = passwordHide.getText().trim();
             passwordShown.setText(password);
         }
-        else if (showPassword) {
+        else {
             password = passwordShown.getText().trim();
             passwordHide.setText(password);
         }
@@ -79,9 +117,5 @@ public class AuthController {
         passwordShown.setVisible(!showPassword);
 
         showPassword = !showPassword;
-    }
-
-    public void checkUser(String login, String password) {
-
     }
 }
--- src/main/java/com/pharmacy/PharmacyApplication.java
+++ src/main/java/com/pharmacy/PharmacyApplication.java
@@ -7,13 +7,13 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-
 public class PharmacyApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-
         FXMLLoader fxmlLoader = new FXMLLoader(PharmacyApplication.class.getResource("/authorization.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Авторизация");
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
