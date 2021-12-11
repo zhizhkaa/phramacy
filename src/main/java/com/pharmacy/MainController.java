545c36024e91e312e1a400b3fdab2e1c6d9684b2
memakh <misha.mahmutov@gmail.com>
2021-12-11 20:46:11 +0300

    Работа с данными - Система удаления, изменения, вставки новых строк.

--- src/main/java/com/pharmacy/MainController.java
+++ src/main/java/com/pharmacy/MainController.java
@@ -3,16 +3,22 @@ package com.pharmacy;
 import com.pharmacy.classes.User;
 import com.pharmacy.classes.MySQLDriver;
 
-import java.util.Map;
-import java.util.HashMap;
+import java.util.*;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import org.json.JSONException;
+import org.w3c.dom.Text;
+
 
 public class MainController {
 
@@ -109,7 +115,7 @@ public class MainController {
 
         // TODO Привязать Email
 
-    // Для соответвствия имёе таблиц к запросам на их вывод
+    // Для соответвствия имён таблиц к их именам в БД
     static private Map<String, String> tableChoiceQueries = new HashMap<String, String>();
     // Работа с данными
     public void onTablesButtonPressed(ActionEvent event) {
@@ -122,75 +128,172 @@ public class MainController {
         // TODO сделать заполнение tableChoice при инициализации формы MainController
         if (tableChoice.getItems().isEmpty()) {
             tableChoice.getItems().add("Активное вещество");
-            tableChoiceQueries.put("Активное вещество", "SELECT * FROM active_substance");
+            tableChoiceQueries.put("Активное вещество", "active_substance");
             tableChoice.getItems().add("Состав акт. веществ препарата");
-            tableChoiceQueries.put("Состав акт. веществ препарата", "SELECT * FROM active_substance_comp");
+            tableChoiceQueries.put("Состав акт. веществ препарата", "active_substance_comp");
             tableChoice.getItems().add("Вспомогательное вещество");
-            tableChoiceQueries.put("Вспомогательное вещество", "SELECT * FROM secondary_substance");
+            tableChoiceQueries.put("Вспомогательное вещество", "secondary_substance");
             tableChoice.getItems().add("Состав всп. веществ препарата");
-            tableChoiceQueries.put("Состав всп. веществ препарата", "SELECT * FROM secondary_substance_comp");
+            tableChoiceQueries.put("Состав всп. веществ препарата", "secondary_substance_comp");
             tableChoice.getItems().add("Исследование");
-            tableChoiceQueries.put("Исследование", "SELECT * FROM research");
+            tableChoiceQueries.put("Исследование", "research");
             tableChoice.getItems().add("Лекарственное средство");
-            tableChoiceQueries.put("Лекарственное средство", "SELECT * FROM medicine");
+            tableChoiceQueries.put("Лекарственное средство", "medicine");
             tableChoice.getItems().add("Поставщик веществ");
-            tableChoiceQueries.put("Поставщик веществ", "SELECT * FROM supplier");
+            tableChoiceQueries.put("Поставщик веществ", "supplier");
             tableChoice.getItems().add("Поставка веществ");
-            tableChoiceQueries.put("Поставка веществ", "SELECT * FROM supplies");
+            tableChoiceQueries.put("Поставка веществ", "supplies");
             tableChoice.getItems().add("Производственный цех");
-            tableChoiceQueries.put("Производственный цех", "SELECT * FROM workshops");
+            tableChoiceQueries.put("Производственный цех", "workshops");
             tableChoice.getItems().add("Должности");
-            tableChoiceQueries.put("Должности", "SELECT * FROM positions");
+            tableChoiceQueries.put("Должности", "positions");
             tableChoice.getItems().add("Сотрудники");
-            tableChoiceQueries.put("Сотрудники", "SELECT * FROM employees");
+            tableChoiceQueries.put("Сотрудники", "employees");
             tableChoice.getItems().add("Партия препарата");
-            tableChoiceQueries.put("Партия препарата", "SELECT * FROM medicine_batch");
+            tableChoiceQueries.put("Партия препарата", "medicine_batch");
             tableChoice.getItems().add("Производство препарата");
-            tableChoiceQueries.put("Производство препарата", "SELECT * FROM medicine_production");
+            tableChoiceQueries.put("Производство препарата", "medicine_production");
             tableChoice.getItems().add("Операции");
-            tableChoiceQueries.put("Операции", "SELECT * FROM operations");
+            tableChoiceQueries.put("Операции", "operations");
             tableChoice.getItems().add("Операции по складу");
-            tableChoiceQueries.put("Операции по складу", "SELECT * FROM warehouse_operations");
+            tableChoiceQueries.put("Операции по складу", "warehouse_operations");
             tableChoice.getItems().add("Склад. партии препарата");
-            tableChoiceQueries.put("Склад. партии препарата", "SELECT * FROM medicine_warehouse");
+            tableChoiceQueries.put("Склад. партии препарата", "medicine_warehouse");
             tableChoice.getItems().add("Заказчик");
-            tableChoiceQueries.put("Заказчик", "SELECT * FROM customers");
+            tableChoiceQueries.put("Заказчик", "customers");
             tableChoice.getItems().add("Сбыт");
-            tableChoiceQueries.put("Сбыт", "SELECT * FROM sales");
+            tableChoiceQueries.put("Сбыт", "sales");
             tableChoice.getItems().add("Сбыт партии");
-            tableChoiceQueries.put("Сбыт партии", "SELECT * FROM medicine_sale");
+            tableChoiceQueries.put("Сбыт партии", "medicine_sale");
         }
     }
     @FXML private Pane tables;
-        @FXML private TableView tvTest;
+        @FXML private TableView<String> tvTest;
         @FXML private ChoiceBox tableChoice;
+        @FXML private Button saveAllButton;
 
     // Для связи с сервером БД
-    private MySQLDriver driver = new MySQLDriver("jdbc:mysql://localhost:3306/pharmacy", "root", "mikeqwer2246");
+    // TODO Придумать для url, root, password ввод
+    String mysql_url = "jdbc:mysql://localhost:3306/pharmacy";
+    String mysql_user = "root";
+    String mysql_pass = "mikeqwer2246";
+
     // Для вывода таблиц в текущем окне
     public void onDisplayTableButtonPressed(ActionEvent event) {
+        /* TODO Вывод окна с подтверждением на смену таблицы без сохранения
+            ИЛИ (сложнее) сделать, чтобы появлялся новый элемент TableView */
+        String tableName = tableChoiceQueries.get(tableChoice.getSelectionModel().getSelectedItem());
+        MySQLDriver driver = new MySQLDriver(mysql_url, mysql_user, mysql_pass, tableName);
         // Удаляем существующие данные в табл
         tvTest.getItems().clear();
         tvTest.getColumns().clear();
-        driver.buildData(tvTest, tableChoiceQueries.get(tableChoice.getSelectionModel().getSelectedItem()));
+        driver.buildData(tvTest);
+        saveAllButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                driver.executePreparedQueries();
+            }
+        });
     }
 
-    // Для вывода таблиц в новом окне
+    // Для вывода таблицы в новом окне
     public void onDisplayWindowedTableButtonPressed(ActionEvent event) {
+        String tableName = tableChoiceQueries.get(tableChoice.getSelectionModel().getSelectedItem());
+        MySQLDriver driver = new MySQLDriver(mysql_url, mysql_user, mysql_pass, tableName);
         // Подготовка окна
-        StackPane layout = new StackPane();
+        VBox layout = new VBox();
         Scene scene = new Scene(layout, 600, 400);
         Stage stage = new Stage();
-        stage.setTitle(tableChoice.getSelectionModel().getSelectedItem().toString());
+        stage.setTitle(tableChoice.getSelectionModel().getSelectedItem().toString() + " - " + tableName);
         stage.setScene(scene);
-        TableView tw = new TableView();
-        // Добавление таблицы
-        layout.getChildren().add(tw);
+        // Подготовка TableView
+        TableView tv = new TableView();
+        tv.setEditable(true);
+        // MenuBar
+        MenuBar menuBar = new MenuBar();
+        Menu menu = new Menu("Таблица");
+        menuBar.getMenus().add(menu);
+        MenuItem save = new MenuItem("Сохранить");
+        MenuItem delete = new MenuItem("Удалить строку");
+        MenuItem insert = new MenuItem("Новая строка...");
+        menu.getItems().add(save);
+        menu.getItems().add(delete);
+        menu.getItems().add(insert);
+        // Кнопка сохранения - вызывает метод, который обрабатывает выполнение сохранённых команд драйвера
+        save.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                driver.executePreparedQueries();
+            }
+        });
+        // Кнопка удаления - фиксирует изменение в структуре данных драйвера data и добавляет в очередь команд
+        delete.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                Object selectedItem = tv.getSelectionModel().getSelectedItem();
+                driver.deleteRow(tv, tv.getItems().indexOf(selectedItem));
+                tv.getItems().remove(selectedItem);
+            }
+        });
+        // Кнопка вставки - аналогично кнопке удаления
+        // Для вставки вызывает новую форму ввода с кол-вом полей таблицы драйвера, после нажатия ОК фиксирует изменения
+        //и добавляет команду вставки в очередь команд
+        insert.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                // Для последующей вставки
+                List<TextField> inputs = new ArrayList<>(); // список полей вставки
+                List<TableColumn> columns = new ArrayList<>(); // список столбцов таблицы
+                // Создаём окно со вставкой
+                VBox newRow = new VBox();
+                Stage inputStage = new Stage();
+                Scene inputScene = new Scene(newRow, 400, 300);
+                inputStage.initModality(Modality.WINDOW_MODAL);
+                inputStage.initOwner(layout.getScene().getWindow());
+                inputStage.setScene(inputScene);
+                // Кнопка ОК - отправка запроса
+                Button ok = new Button("ОК");
+                ok.setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent event) {
+                        HashMap<String, Object> values = new HashMap(); // для хранения пары имя столбца, новое значение
+                        // Если хотя бы одно поле заполнено
+                        if (!inputs.isEmpty()) {
+                            ObservableList new_row = FXCollections.observableArrayList(); // новая строка для структуры данных
+                            Iterator<TableColumn> i_col = columns.iterator();
+                            for (Iterator<TextField> i_text = inputs.iterator(); i_text.hasNext() && i_col.hasNext();) {
+                                TableColumn e_col = i_col.next();
+                                TextField e_text = i_text.next();
+                                values.put(e_col.getText(), e_text.getText());
+                                new_row.add(e_text.getText());
+                            }
+                            // Сохранение как запроса к бд
+                            driver.inputRow(tv, values, new_row);
+                            // Выход из формы заполнения вставки
+                            inputStage.close();
+                        }
+
+                    }
+                });
+                // Итерация по всем полям - заполнение списков
+                for (Iterator<TableColumn> i = tv.getColumns().iterator(); i.hasNext();) {
+                    TableColumn e = i.next();
+                    TextField tf = new TextField(e.getText());
+                    inputs.add(tf);
+                    columns.add(e);
+                    newRow.getChildren().add(tf);
+                }
+                newRow.getChildren().add(ok);
+                inputStage.show();
+            }
+        });
+        // Добавление элементов
+        layout.getChildren().add(menuBar);
+        layout.getChildren().add(tv);
         // Заполнение таблицы
-        driver.buildData(tw, tableChoiceQueries.get(tableChoice.getSelectionModel().getSelectedItem()));
+        driver.buildData(tv);
         stage.show();
     }
-
 
     // Работа с отчётами
     public void onReportsButtonPressed(ActionEvent event) {
